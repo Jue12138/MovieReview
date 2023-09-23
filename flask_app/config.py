66d8196b838209b2ed8842f119fad542a5898b34
Jingjing Nie <jnie01@terpmail.umd.edu>
2023-09-23 10:37:31 -0400

    Update gitignore

--- flask_app/config.py
+++
@@ -1,7 +0,0 @@
-# Stores all configuration values
-SECRET_KEY = b'\x020;yr\x91\x11\xbe"\x9d\xc1\x14\x91\xadf\xec'
-MONGODB_HOST = "mongodb://localhost:27017/movie_review"
-GOOGLE_CLIENT_ID = '300176334730-7cgr5ushv0b21n3c37ijrkcei98dsk42.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = '-BTrnEAqtS3d0RrQPsQoFuCz'
-OAUTHLIB_INSECURE_TRANSPORT = '1'  
-OMDB_API_KEY = 'de69e125'
